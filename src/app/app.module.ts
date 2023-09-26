fbf7fc058b4ed77bc2994ff0e5dac6ca261c65fc
Sallys Carlos <sallys.carlos93@gmail.com>
2023-09-26 17:55:58 -0300

    chore: add settings of progress bar dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -9,6 +10,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SystemMonitorComponent } from './pages/system-monitor/system-monitor.component';
+import { DataService } from './services/data.service';
+import { HttpClientModule } from '@angular/common/http';
+import { IgxProgressBarModule } from 'igniteui-angular';
 
 @NgModule({
   declarations: [AppComponent, CardComponent, SystemMonitorComponent],
@@ -18,8 +22,11 @@ import { SystemMonitorComponent } from './pages/system-monitor/system-monitor.co
     BrowserAnimationsModule,
     MatCardModule,
     MatProgressSpinnerModule,
+    HttpClientModule,
+    IgxProgressBarModule,
+    MatIconModule,
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
