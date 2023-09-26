557729be6dff0909215665aed1941a6a1eee5086
Caio Rocha <rochacaiodev@gmail.com>
2023-09-26 01:00:22 -0300

    feature: add routing (for future purposes, if needed)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SystemMonitorComponent } from './pages/system-monitor/system-monitor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'system-monitor', component: SystemMonitorComponent },
+  { path: '', redirectTo: '/system-monitor', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
